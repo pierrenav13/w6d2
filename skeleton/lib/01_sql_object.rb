b8fd4ea8541ca6609edc1f1a8347e7974aa83690
pierre <pnavarin@gmail.com>
2021-09-14 17:47:58 -0400

    Finished up to initialize

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -5,18 +5,38 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns unless @columns.nil?
+    array = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT 0
+    SQL
+    array.map!{|ele| ele.to_sym}
+    @columns = array
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    t = self.to_s.tableize
+    return t unless t[-1] != 's'
+    self.to_s.downcase + 's'
   end
 
   def self.all
@@ -36,7 +56,8 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes = {} unless @attributes
+    @attributes
   end
 
   def attribute_values
